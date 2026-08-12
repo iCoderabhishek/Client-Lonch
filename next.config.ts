c76c7f8a481386747f9a29c8f5f0978d06061147
Abhishek Jha <iamabhishek1310@gmail.com>
2026-08-12 18:34:18 +0530

    Added async rewrite config to proxy prevent backend exposure

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
+  output: process.env.VERCEL ? undefined : 'standalone',
   reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.BACKEND_API_URL}/api/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
